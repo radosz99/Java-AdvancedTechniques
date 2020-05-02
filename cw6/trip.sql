db40bbd8eeae0a2eef6e69dbfd23be70726da2c6
radosz99 <47248979+radosz99@users.noreply.github.com>
2020-05-03 01:21:26 +0200

    Add demo of MySQL connection cw6

---
+++ cw6/trip.sql
@@ -0,0 +1,56 @@
+﻿create table trip (
+	id INT,
+	name VARCHAR(50),
+	author_id INT,
+	date DATE
+);
+insert into trips (id, name, author_id, date) values (1, 'Rustenburg', 218, '02/20/2009');
+insert into trips (id, name, author_id, date) values (2, 'Stockholm', 296, '09/17/2015');
+insert into trips (id, name, author_id, date) values (3, 'Brok', 485, '12/04/2000');
+insert into trips (id, name, author_id, date) values (4, 'Guaratinguetá', 59, '01/03/2004');
+insert into trips (id, name, author_id, date) values (5, 'Dar es Salaam', 543, '09/22/2005');
+insert into trips (id, name, author_id, date) values (6, 'Pereira Barreto', 423, '03/29/2004');
+insert into trips (id, name, author_id, date) values (7, 'Seaforth', 987, '04/21/2008');
+insert into trips (id, name, author_id, date) values (8, 'Koson', 922, '03/09/2020');
+insert into trips (id, name, author_id, date) values (9, 'Chaykovskaya', 472, '11/04/2015');
+insert into trips (id, name, author_id, date) values (10, 'Mazongshan', 369, '01/23/2016');
+insert into trips (id, name, author_id, date) values (11, 'Dazan', 380, '05/03/2008');
+insert into trips (id, name, author_id, date) values (12, 'Unai', 278, '12/04/2018');
+insert into trips (id, name, author_id, date) values (13, 'Seririt', 664, '02/14/2005');
+insert into trips (id, name, author_id, date) values (14, 'Songping', 914, '01/29/2012');
+insert into trips (id, name, author_id, date) values (15, 'Pretana', 19, '11/10/2019');
+insert into trips (id, name, author_id, date) values (16, 'Quelimane', 822, '06/07/2004');
+insert into trips (id, name, author_id, date) values (17, 'Kourou', 414, '06/15/2002');
+insert into trips (id, name, author_id, date) values (18, 'Stari Grad', 123, '01/24/2019');
+insert into trips (id, name, author_id, date) values (19, 'Cahors', 737, '11/18/2007');
+insert into trips (id, name, author_id, date) values (20, 'Tiadiaye', 870, '03/21/2019');
+insert into trips (id, name, author_id, date) values (21, 'Valuyki', 400, '12/27/2018');
+insert into trips (id, name, author_id, date) values (22, 'Xikou', 368, '01/12/2014');
+insert into trips (id, name, author_id, date) values (23, 'Santol', 984, '05/10/2004');
+insert into trips (id, name, author_id, date) values (24, 'Yangce', 44, '11/19/2003');
+insert into trips (id, name, author_id, date) values (25, 'Valle de La Pascua', 15, '12/08/2015');
+insert into trips (id, name, author_id, date) values (26, 'Oxelosund', 56, '07/20/2014');
+insert into trips (id, name, author_id, date) values (27, 'Wschowa', 765, '01/29/2010');
+insert into trips (id, name, author_id, date) values (28, 'Arendal', 294, '09/16/2010');
+insert into trips (id, name, author_id, date) values (29, 'Bahe', 400, '07/28/2006');
+insert into trips (id, name, author_id, date) values (30, 'Haozhai', 944, '11/03/2001');
+insert into trips (id, name, author_id, date) values (31, 'Longquan', 529, '09/15/2013');
+insert into trips (id, name, author_id, date) values (32, 'Vuhlehirs’k', 415, '02/06/2006');
+insert into trips (id, name, author_id, date) values (33, 'Tanahmerah', 647, '10/01/2010');
+insert into trips (id, name, author_id, date) values (34, 'Kalasin', 821, '03/04/2004');
+insert into trips (id, name, author_id, date) values (35, 'Gordon Town', 993, '04/06/2013');
+insert into trips (id, name, author_id, date) values (36, 'Nedasov', 920, '06/02/2006');
+insert into trips (id, name, author_id, date) values (37, 'Yarim', 604, '03/15/2007');
+insert into trips (id, name, author_id, date) values (38, 'Dobra', 45, '12/16/2004');
+insert into trips (id, name, author_id, date) values (39, 'Aurora', 173, '10/13/2003');
+insert into trips (id, name, author_id, date) values (40, 'Talun', 108, '09/05/2019');
+insert into trips (id, name, author_id, date) values (41, 'Longtang', 53, '11/19/2010');
+insert into trips (id, name, author_id, date) values (42, 'Chuanbu', 254, '03/31/2016');
+insert into trips (id, name, author_id, date) values (43, 'Baihe', 145, '04/04/2004');
+insert into trips (id, name, author_id, date) values (44, 'Bombardopolis', 265, '04/11/2015');
+insert into trips (id, name, author_id, date) values (45, 'Chartres', 158, '03/31/2015');
+insert into trips (id, name, author_id, date) values (46, 'Alor Setar', 914, '11/08/2009');
+insert into trips (id, name, author_id, date) values (47, 'Muara', 518, '09/13/2001');
+insert into trips (id, name, author_id, date) values (48, 'Monte Mor', 461, '08/10/2012');
+insert into trips (id, name, author_id, date) values (49, 'Shiyuetian', 513, '09/06/2009');
+insert into trips (id, name, author_id, date) values (50, 'Yubileyny', 951, '10/27/2017');
